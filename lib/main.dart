ccffc9b5092c0e1a368b013455c74de39d4e084b
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 20:57:39 +0800

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'To Do App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
