f05571e014b33212ca7fbb2bc61b4ae21dbe0543
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 23:35:47 +0800

    Update TodoTile widget with onDelete callback in todo_tile.dart

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class TodoTile extends StatelessWidget {
   final String taskName;
   final bool isDone;
   Function(bool?)? onChanged;
+  Function(BuildContext)? onDelete;
 
   TodoTile({
     super.key,
     required this.taskName,
     required this.isDone,
     required this.onChanged,
+    required this.onDelete,
   });
 
   @override
@@ -20,31 +23,44 @@ class TodoTile extends StatelessWidget {
         left: 12.0,
         right: 12.0,
       ),
-      child: Container(
-        padding: const EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: Colors.blueAccent[100],
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // Checkbox
-            Checkbox(
-              value: isDone,
-              onChanged: onChanged,
-              activeColor: Colors.black,
-            ),
-
-            // Task Name
-            Text(
-              taskName,
-              style: TextStyle(
-                decoration: isDone
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
-              )
-            ),
+            SlidableAction(
+              onPressed: onDelete,
+              icon: Icons.delete,
+              backgroundColor: Colors.redAccent,
+              borderRadius: BorderRadius.circular(10),
+            )
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            color: Colors.blueAccent[100],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Row(
+            children: [
+              // Checkbox
+              Checkbox(
+                value: isDone,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+        
+              // Task Name
+              Text(
+                taskName,
+                style: TextStyle(
+                  decoration: isDone
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                )
+              ),
+            ],
+          ),
         ),
       ),
     );
