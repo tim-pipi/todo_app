3dc181f95228025310e993d809f1a7215b50a7e6
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 23:01:41 +0800

    Update DialogBox widget with new features and styling

--- lib/widgets/dialog_box.dart
+++ lib/widgets/dialog_box.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final controller;
+
+  const DialogBox({super.key, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return const AlertDialog();
+    return AlertDialog(
+      backgroundColor: Colors.blue,
+      content: Container(
+        height: 120,
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Add a New Task",
+              )
+            ),
+          ]
+        )
+      ),
+      actions: [
+        TextButton(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.blue[100]),
+          ),
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Add'),
+        ),
+      ],
+    );
   }
 }
