4a12deacb8fdc416893ffe45f962323458dc6af1
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-22 00:12:30 +0800

    Add database.dart

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -4,5 +4,30 @@ class TodoDatabase {
   List todoList = [];
 
   // Reference the Hive Box
-  final _myBox = Hive.openBox('todoList');
+  final _myBox = Hive.box('todoList');
+
+  void createInitialData() {
+    todoList = [
+      {
+        'taskName': 'Buy groceries',
+        'isDone': false,
+      },
+      {
+        'taskName': 'Clean the house',
+        'isDone': false,
+      },
+      {
+        'taskName': 'Cook dinner',
+        'isDone': false,
+      },
+    ];
+  }
+
+  void loadData() {
+    todoList = _myBox.get('TODO');
+  }
+
+  void saveData() {
+    _myBox.put('TODO', todoList);
+  }
 }
