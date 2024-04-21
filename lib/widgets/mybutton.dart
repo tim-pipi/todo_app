bf01d127d050c489d2401951bbcfc10c0f4b4754
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 23:21:45 +0800

    Add MyButton widget

---
+++ lib/widgets/mybutton.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  VoidCallback onPressed;
+
+  MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MaterialButton(
+        onPressed: onPressed,
+        color: Colors.blue[100],
+        child: Text(text),
+      ),
+    );
+  }
+}
