6b0b98b63324b9acb98e6df5b42679c6142a50eb
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 23:46:52 +0800

    Add Hive database initialization and open box in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  // Init the hive database
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('todoList');
+
   runApp(const MyApp());
 }
 
