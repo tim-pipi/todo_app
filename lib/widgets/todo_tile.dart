e5bf1fca488aa8471a7200c3c69e2881af360420
Timothy Putra Prasetio <timothypp02@gmail.com>
2024-04-21 22:44:32 +0800

    Update padding in TodoTile widget

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -15,7 +15,11 @@ class TodoTile extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(12.0),
+      padding: const EdgeInsets.only(
+        top: 12.0,
+        left: 12.0,
+        right: 12.0,
+      ),
       child: Container(
         padding: const EdgeInsets.all(12.0),
         decoration: BoxDecoration(
